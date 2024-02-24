1eb831b2b780f4f6b3d6c03edf92e5fbfefcf33e
Raphael Correa <raphaelcorreaesilva@gmail.com>
2024-02-24 13:03:36 -0300

    add more images of me to about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,25 +1,39 @@
 import { useState } from 'react';
 import PageHeader from './PageHeader';
 
+const images = ['me1.JPG', 'me2.JPG', 'me3.JPG', 'me4.JPG'];
+
 function About() {
   document.title = 'About | RCS Photography';
-  const [ready, setReady] = useState<boolean>();
-  
+
+  const [imageIsReady, setImageIsReady] = useState<boolean[]>([]);
+
+  const setImageIsReadyToTrue = (index) => {
+    const newReady = [...imageIsReady];
+    newReady[index] = true;
+    setImageIsReady(newReady);
+  }
+
   return <div id="about">
     <div className="container">
       <PageHeader title="About" subtitle={"by Raphael Corrêa"}/>
       <div className="body">
         <p>This is a portfolio for my photography, one of my biggest hobbies once upon a time.</p>
         <p>I shoot mostly in manual and record in raw format. I also use CaptureOne to edit my work.</p>
-        <p>This website was written in React with Typescript and is hosted on AWS.</p>
+        <p>This website was written in React with Typescript and is hosted on AWS and IBM Cloud.</p>
         <p>The source code is on <a href='https://www.github.com/raphael-correa-ng/rcsphoto' target='_blank' rel='noreferrer'>GitHub</a>.</p>
         <p>Thanks for viewing!</p>
-        <div className="image-container">
-          <img 
-            src="me.jpg"
-            alt=""
-            className={ready ? 'image-ready' : 'image-not-ready'} 
-            onLoad={() => setReady(true)}/>
+        <div className="images-container">
+          {
+            images.map((image, index) =>
+              <img
+                key={index}
+                src={image}
+                alt=""
+                className={imageIsReady[index] ? 'image-ready' : 'image-not-ready'}
+                onLoad={() => setImageIsReadyToTrue(index)}/>
+            )
+          }
         </div>
       </div>
     </div>
